2c62a48c64cc2fff9e98ef8f30ab5b473704d332
junessatthentia <100451626+junessatthentia@users.noreply.github.com>
2022-06-15 19:58:58 -0400

    Cirrus Platform Redesign
    Record In View Template
    - textareas, inputs, file uploader, datepicker

---
+++ js/cirrus.js
@@ -0,0 +1,8 @@
+tinymce.init({
+  selector: 'textarea#editor',
+  skin: 'bootstrap',
+  height: 168,
+  plugins: 'lists, link, image, media',
+  toolbar: 'bold italic underline bullist numlist | alignleft aligncenter alignright alignjustify | link image | table | removeformat',
+  menubar: false,
+  });
